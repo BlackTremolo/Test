b0502f7fb5be99eb2edb56cf1f133ec4b6ad939c
Mihser <mihsee436@gmail.com>
2025-05-30 20:15:36 +0800

    Add new code

--- app.rb
+++ app.rb
@@ -1,3 +1,10 @@
 require 'sqlite3'
 
 db = SQLite3::Database.new 'test.sqlite'
+
+db.execute "insert into Cars.sqlite (Cars,Prise) values ('ZaZ', 500)"
+db.execute "select * from Cars.sqlite" do |car|
+	puts car
+	puts '===='
+
+db.close
